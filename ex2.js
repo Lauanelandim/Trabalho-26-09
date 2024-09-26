540c502320f872423133bdea453e534dab19023e
Lauanelandim <landim0lauane@hotmail.com>
2024-09-25 21:35:25 -0300

    Add files via upload

---
+++ ex2.js
@@ -0,0 +1,9 @@
+//Faça um programa que mostre os números pares entre 1 e 100, inclusive.
+//Entrada
+//Neste problema extremamente simples de repetição não há entrada.
+//Saída
+//Imprima todos os números pares entre 1 e 100, inclusive se for o caso, um em cada linha.
+
+for (let par = 2; par <= 100; par += 2) {
+    console.log(par);
+}
---
+++ ex8.js
@@ -0,0 +1,14 @@
+//Leia quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).
+//Entrada
+//O arquivo de entrada contém 4 valores inteiros.
+//Saída
+//Imprima a mensagem DIFERENCA com todas as letras maiúsculas, conforme exemplo abaixo, com um espaço em branco antes e depois da igualdade.
+
+let A = parseInt(5);
+let B = parseInt(6);
+let C = parseInt(7);
+let D = parseInt(8);
+
+let diferenca = A * B - C * D;
+
+console.log(`DIFERENCA = ${diferenca}`);
